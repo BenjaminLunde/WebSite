927846cbd36e12e78f74c87f902c79b0f042c96f
Benjamin Lunde <blu009@post.uit.no>
2021-02-09 22:49:15 +0100

    Datbase
    Fixed 2x

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,15 +84,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
-
-
 if DEVELOPMENT_MODE is True:
     DATABASES = {
         "default": {

647e184beec0b8c01386a13fea8bbe7d1b1fcc13
Benjamin Lunde <blu009@post.uit.no>
2021-02-10 01:40:58 +0100

    Opsi
    Degub fix lol

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =  os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv("DEBUG", "False") == "True"
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 

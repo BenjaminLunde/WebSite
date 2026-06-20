3e46519a26c7d9ffefbf35ecc0a9de0274a6a541
BenjaminLundeStorebrand <benjamin.lunde@storebrand.no>
2026-06-20 20:51:06 +0200

    Run migrations on deploy"

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from .views import SignUpView
 
 
 from . import views
@@ -10,7 +9,6 @@ urlpatterns = [
     path('<int:info_id>/', views.get_one, name="get_one"),
     path('shopping/', views.shopping, name="shopping"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('signup/', SignUpView.as_view(), name='signup'),
     path('account/', views.account, name="account"),
     path('delete/<int:ingredientToShop_id>', views.delete_shop, name='delete'),
     path('delete_all/', views.delete_all, name='delete_all'),
